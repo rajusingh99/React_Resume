a7459439aaaf23c8109bbd1deb310602cb6c06dd
Raju Singh <rajuofficialk@gmail.com>
2023-05-31 12:16:16 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './components/About';
+import Apply from './components/Apply';
+import Books from './components/Books';
+import ComplexForm from './components/ComplexForm';
+import Navbar from './components/Navbar';
+import Register from './components/Register';
+import Typography from './components/Typography';
+import Alert from './components/Alert'
 
 function App() {
+   const [mode, setMode] = useState('light');
+   const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type)=>{
+      setAlert({
+        msg: message,
+        type: type
+      })
+      setTimeout(() => {
+          setAlert(null);
+      }, 1500);
+  }
+
+   const toggleMode = ()=>{
+
+      if(mode === 'light'){
+        setMode('dark');
+        document.body.style.backgroundColor = '#021C31';
+        showAlert("Dark mode has been enabled", "success");
+      }
+      else{
+        setMode('light');
+        document.body.style.backgroundColor = 'white';
+        showAlert("Light mode has been enabled", "success");
+      }
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar toggleMode={toggleMode}/>
+        <Alert alert={alert}/>
+        <Routes>
+          <Route path="/" element={<Typography />} />  
+          <Route path="/about" element={<About />} />
+          <Route path="/apply" element={<Apply />} />
+          <Route path="/books" element={<Books />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/complex-form" element={<ComplexForm />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
